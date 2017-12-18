19a76270eab3b7fff258eed0af9608c3e52797a4
Wayne Grant <waynedgrant@users.noreply.github.com>
2017-12-18 19:30:46 +0000

    Add Awareness

---
+++ src/Awareness.php
@@ -0,0 +1,42 @@
+<?php
+
+# Copyright 2018 Wayne D Grant (www.waynedgrant.com)
+# Licensed under the MIT License
+
+class Awareness {
+
+    public function __construct($awareness_dom) {
+        // e.g. <td><img src="..." alt="awt:4 level:3"></td>
+        $img_alt = $awareness_dom->getElementsByTagName('img')->item(0)->getAttribute('alt');
+        $this->parse_awareness_level($img_alt);
+        $this->parse_awareness_type($img_alt);
+    }
+
+    private function parse_awareness_level($img_alt) {
+        // e.g. level value from "awt:4 level:3"
+        $start_awareness_level_index = strrpos($img_alt, ':') + 1;
+        $awareness_level_length = strlen($img_alt) - $start_awareness_level_index;
+        $this->awarenessLevel = new AwarenessLevel(intval(substr($img_alt, $start_awareness_level_index, $awareness_level_length)));
+    }
+
+    private function parse_awareness_type($img_alt) {
+        if ($this->awarenessLevel->level() == AwarenessLevel::GREEN) {
+            $this->awarenessType = new AwarenessType(AwarenessType::NONE); // Awareness types need to be corrected when level is green
+        } else {
+            // e.g. awt value from "awt:4 level:3"
+            $start_awareness_type_index = strpos($img_alt, ':') + 1;
+            $awareness_type_length = strpos($img_alt, ' ') - $start_awareness_type_index;
+            $this->awarenessType = new AwarenessType(intval(substr($img_alt, $start_awareness_type_index, $awareness_type_length)));
+        }
+    }
+
+    public function awarenessType() {
+        return $this->awarenessType;
+    }
+
+    public function awarenessLevel() {
+        return $this->awarenessLevel;
+    }
+}
+
+?>
---
+++ test/AwarenessTest.php
@@ -0,0 +1,38 @@
+<?php
+
+# Copyright 2018 Wayne D Grant (www.waynedgrant.com)
+# Licensed under the MIT License
+
+class AwarenessTest extends PHPUnit\Framework\TestCase {
+
+    public function test_warning() {
+
+        $awareness_html =
+            '<td width="28">' .
+                '<img border="1" src="http://web.meteoalarm.eu/documents/rss/wflag-l2-t2.jpg" alt="awt:2 level:3">' .
+            '</td>';
+
+        $awareness_dom = DOMDocument::loadHTML($awareness_html);
+
+        $testee = new Awareness($awareness_dom);
+        $this->assertSame(AwarenessType::SNOW_ICE, $testee->awarenessType()->type());
+        $this->assertSame(AwarenessLevel::AMBER, $testee->awarenessLevel()->level());
+    }
+
+    public function test_no_warning() {
+
+        // Any awareness type other than 'None' makes no sense when level is 'Green'
+        $awareness_html =
+            '<td width="28">' .
+                '<img border="1" src="http://web.meteoalarm.eu/documents/rss/wflag-l2-t2.jpg" alt="awt:3 level:1">' .
+            '</td>';
+
+        $awareness_dom = DOMDocument::loadHTML($awareness_html);
+
+        $testee = new Awareness($awareness_dom);
+        $this->assertSame(AwarenessType::NONE, $testee->awarenessType()->type());
+        $this->assertSame(AwarenessLevel::GREEN, $testee->awarenessLevel()->level());
+    }
+}
+
+?>
