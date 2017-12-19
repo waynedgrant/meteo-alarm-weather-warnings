148b034906cd1e8c8f0d99b9d0d8856d56a1b625
Wayne Grant <waynedgrant@users.noreply.github.com>
2017-12-19 14:41:06 +0000

    Add examples to warning

--- src/Warning.php
+++ src/Warning.php
@@ -6,13 +6,14 @@
 class Warning {
 
     public function __construct($awarness_period_row, $description_row, $output_timezone) {
-
+        // e.g. <tr><td> ... awareness ... </td><td> ... period ... </td></tr>
         $awareness_cell = $awarness_period_row->getElementsByTagName('td')->item(0);
         $this->awareness = new Awareness($awareness_cell);
 
         $period_cell = $awarness_period_row->getElementsByTagName('td')->item(1);
         $this->period = new Period($period_cell, $output_timezone);
 
+        // e.g. <tr><td /><td> ... description ... </td></tr>
         $description_cell = $description_row->getElementsByTagName('td')->item(1);
         $this->description = new Description($description_cell);
     }
