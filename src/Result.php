554e03f17721bdda365e252787be0d1009406fa4
Wayne Grant <waynedgrant@users.noreply.github.com>
2017-12-24 19:50:42 +0000

    Add Result with endpoint details

---
+++ src/Result.php
@@ -0,0 +1,33 @@
+<?php
+
+class Result {
+
+    const SERVICE_VERSION = '1.0';
+    const GITHUB_PROJECT_LINK = 'https://github.com/waynedgrant/meteo-alarm-weather-warnings';
+    const COPYRIGHT_NOTICE = 'Copyright © 2018 Wayne D Grant (www.waynedgrant.com)';
+
+    public function __construct($rss) {
+        $this->warnings = new Warnings($rss);
+    }
+
+    private function createServiceUrl() {
+        return 'http'.(!empty($_SERVER['HTTPS'])?'s':'').'://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+    }
+
+    public function serialize() {
+        foreach ($this->regions as $region) {
+            $regions[] = $region->serialize();
+        }
+
+        return [
+            'endpoint' => [
+                'url' => $this->createServiceUrl(),
+                'version' => self::SERVICE_VERSION,
+                'github_project' => self::GITHUB_PROJECT_LINK,
+                'copyright' => self::COPYRIGHT_NOTICE
+            ],
+            'warnings' => $this->warnings->serialize()
+        ];
+    }
+}
+?>
